8c44625417611cd8b28b1635f158718c7eac47d2
Ramitphi <18ucs144@lnmiit.ac.in>
2024-08-15 01:40:07 +0530

    added airstack

--- pages/api/frames/frames.ts
+++ pages/api/frames/frames.ts
@@ -1,5 +1,16 @@
+import { farcasterHubContext } from "frames.js/middleware";
 import { createFrames } from "frames.js/next/pages-router/server";
 
 export const frames = createFrames({
   basePath: "/api/frames",
+  middleware: [
+    farcasterHubContext({
+      hubHttpUrl: "https://hubs.airstack.xyz",
+      hubRequestOptions: {
+        headers: {
+          "x-airstack-hubs": "1108ca72f6a414da788a0bd485866ca62" as string,
+        },
+      },
+    }),
+  ],
 });
