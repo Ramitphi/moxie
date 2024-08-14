b8a5449251c99a7055d02e9f73733b09d5365979
Ramitphi <18ucs144@lnmiit.ac.in>
2024-08-15 01:29:27 +0530

    checking

--- pages/api/stats/index.tsx
+++ pages/api/stats/index.tsx
@@ -1,16 +1,13 @@
 /* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next/pages-router/server";
-import { frames } from "./frames";
+import { frames } from "../frames/frames";
 
 const handleRequest = frames(async (ctx) => {
   return {
     image: "https://i.postimg.cc/Rhsk3zPB/moxie.png",
 
     buttons: [
-      <Button
-        action="post_redirect"
-        target={"https://moxie-five.vercel.app/api/stats"}
-      >
+      <Button action="link" target={"https://huddle01.com/"}>
         Value of my locked Moxie
       </Button>,
     ],
